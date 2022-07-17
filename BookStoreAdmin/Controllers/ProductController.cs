acb5a3f29cffff10cbc57c2de5d5cdb0d8b31fdd
TrungNguyen140526 <90686741+TrungNguyen140526@users.noreply.github.com>
2022-07-17 17:04:11 +0700

    Add Product page

---
+++ BookStoreAdmin/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAdmin.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
